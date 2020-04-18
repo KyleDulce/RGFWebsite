b123edc0b951424cdc68b860ad56d22289485093
KyleDulce <kylejacob78@gmail.com>
2020-04-18 18:29:26 -0400

    Added Debug page

--- app.js
+++ app.js
@@ -12,17 +12,21 @@ app.get('/', function(req, res) {
 app.get('/game', function(req, res) {
   res.sendFile(__dirname + '/client/html/game.html');
 });
+app.get('/debug', function(req, res) {
+  res.sendFile(__dirname + '/client/html/TestPage.html');
+});
 app.use('/client',express.static(__dirname + '/client'));
 
 serv.listen(port);
+
+console.log('Server Started')
 
 var io = require('socket.io')(serv,{});
 
 io.sockets.on('connection', function(socket){
   console.log('socket connection');
   
-  socket.on('test', function(data) {
-    console.log('it works');
+  socket.on('chatSend', function(data) {
     console.log(data.message);
     socket.broadcast.emit('chatRecieve', {
       message: data.message
